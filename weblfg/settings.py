e1429887737bf52266d103f2659f8806d48cbb17
Jarauta1 <diegojarauta8@gmail.com>
2021-10-14 18:21:49 +0200

    firts config

--- weblfg/settings.py
+++ weblfg/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'apilfg.apps.ApilfgConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
