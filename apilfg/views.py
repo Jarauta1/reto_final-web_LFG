e1f5df35276190c3bb055d4cd113636db3834f6f
Jarauta1 <diegojarauta8@gmail.com>
2021-10-28 19:49:18 +0200

    Added first view

--- apilfg/views.py
+++ apilfg/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+# Creamos las vistas
 
-# Create your views here.
+# Importamos las vistas
+from rest_framework import viewsets
+
+# Importamos los serializers creados
+from .serializers import ServerSerializer, GroupSerializer, UsersSerializer, GroupSerializer, MensageSerializer, ChannelSerializer
+
+# Importamos los modelos creados
+from .models import Server, Group, Users, Mensage, Channel
+
+# Indicamos lo que debe serializar
+class ServerViewSet(viewsets.ModelViewSet):
+    queryset = Server.objects.all().order_by('server_id')
+    serializer_class = ServerSerializer
